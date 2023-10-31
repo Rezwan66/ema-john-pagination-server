59b1101c73871738720f556c1805e6ca3f6e9e7b
Rezwan66 <shaikhrezwan66@gmail.com>
2023-11-01 04:40:46 +0600

    created a get api to get total count of products in the DB

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zjzxbzp.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -33,6 +30,11 @@ async function run() {
     app.get('/products', async (req, res) => {
       const result = await productCollection.find().toArray();
       res.send(result);
+    })
+
+    app.get('/productCount', async (req, res) => {
+      const count = await productCollection.estimatedDocumentCount();
+      res.send({ count });
     })
 
     // Send a ping to confirm a successful connection
